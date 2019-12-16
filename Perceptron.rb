6555129bd526209a75a8f3825f57b10ff9600d32
Gerardo Manuel Chávez Larios <gerardo.larios69@gmail.com>
2019-12-16 16:01:23 -0600

    [ADD] private methods into Perceptron

--- Perceptron.rb
+++ Perceptron.rb
@@ -1,10 +1,24 @@
 class Perceptron
-    def initialize()
-        
+    def initialize(data, **options)
+        @data = data
+        @umbral = get_attr(options, "umbral")
+        @rate = get_attr(options, "rate")
+        @weights = get_attr(options, "weights", [rand(), rand()])
+        @learning = true
+        @iterations = 0
     end
 
     def learn
-        
+        while @learning
+            @iterations += 1
+            @learning = false
+            @data.each do |pattern|
+                a = calculate_total_input(pattern)
+                y = activation_function(a)
+                d = get_d(pattern)
+                change_weights(d, y, pattern)                
+            end
+        end
     end
 
     def view_net
@@ -13,5 +27,47 @@ class Perceptron
 
     def info
         
+    end
+
+    private
+
+    def get_attr(options, name, default = rand())
+        options.has_key?(name) ? options[name] : default
+    end
+
+    def activation_function(a)
+        a > 0 ? 1 : 0
+    end
+
+    def calculate_total_input(pattern)
+        num_entries = pattern.length - 1
+        a = (0..num_entries).inject(0) do |a, entry|
+            a + (pattern[entry] * @weights[entry])
+        end
+        @umbral + a
+    end
+
+    def change_weights(d, y, pattern)
+        error = d - y
+        if error != 0
+            num_entries = pattern.length - 1
+            calculate_weights(num_entries, error, pattern)
+            calculate_umbral(error)
+            @learning = true
+        end
+    end
+
+    def calculate_weights(num_entries, error, pattern)
+        (0..num_entries).each do |entry|
+            @weights[entry] = @weights[entry] + @rate * error * pattern[entry]
+        end
+    end
+
+    def calculate_umbral(error)
+        @umbral = @umbral + rate * error * 1
+    end
+
+    def get_d(pattern)
+        pattern[pattern.length - 1]
     end
 end
--- main.rb
+++ main.rb
@@ -2,3 +2,5 @@ require_relative "Perceptron"
 
 AND_GATE = [[1,1,1], [1,0,0], [0,1,0], [0,0,0]]
 OR_GATE = [[1,1,1], [1,0,1], [0,1,1], [0,0,0]]
+
+p = Perceptron.new(AND_GATE)
